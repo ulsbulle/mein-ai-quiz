4c623dfa5659047b2b0f4d2067ee07615eaa2acf
ulsbulle <ulsbullterrier@web.de>
2026-03-28 23:59:54 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,9 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+});
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public')); // Deine HTML/JS/CSS Dateien
