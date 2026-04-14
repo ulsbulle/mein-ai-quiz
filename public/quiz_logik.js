8677caf8d9d787619fdf9d3c04b5cd2122d913e2
ulsbulle <ulsbullterrier@web.de>
2026-04-14 18:06:41 +0200

    Update quiz_logik.js

--- public/quiz_logik.js
+++ public/quiz_logik.js
@@ -49,7 +49,7 @@ async function startQuizGeneration() {
 		
 		//Timeout-Schutz vorbereiten 30 Sektionen
 		const controller = new AbortController();
-        const timeoutId = setTimeout(() => controller.abort(), 30000);		
+        const timeoutId = setTimeout(() => controller.abort(), 60000);		
 				
 		const res = await fetch('/api/quiz', { 
 			method: 'POST', 
